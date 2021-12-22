1671cb71f954bcc3cf8935d69d5c7b42d1d5f9d4
Nguyen Hua Hung <63572298+huahungnguyen121@users.noreply.github.com>
2021-12-22 17:21:20 +0700

    Create invoice detail page and tweak some pages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,9 @@
-import { faHome, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faShoppingCart, faUser, faSignInAlt, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Menu } from "antd";
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./styles.css"
 
 AppHeader.propTypes = {};
 
@@ -10,17 +11,32 @@ function AppHeader(props) {
     const location = useLocation();
     const path = location.pathname.split("/");
     return (
-        <Menu selectedKeys={path[1]} mode="horizontal">
-            <Menu.Item key="menu" icon={<FontAwesomeIcon icon={faHome} />}>
-                <Link to="/menu/categories">Sản phẩm</Link>
-            </Menu.Item>
-            <Menu.Item key="profile" icon={<FontAwesomeIcon icon={faUser} />}>
-                <Link to="/profile">Hồ sơ người dùng</Link>
-            </Menu.Item>
-            <Menu.Item key="cart" icon={<FontAwesomeIcon icon={faShoppingCart} />}>
-                <Link to="/cart">Giỏ hàng</Link>
-            </Menu.Item>
-        </Menu>
+        <div className="navigation-bar">
+            <div className="navigation-left">
+                <span><Link to="/menu/categories">ADB2_3 Project 2</Link></span>
+                <Menu selectedKeys={path[1]} mode="horizontal">
+                    <Menu.Item key="menu" icon={<FontAwesomeIcon icon={faHome} />}>
+                        <Link to="/menu/categories">Sản phẩm</Link>
+                    </Menu.Item>
+                    <Menu.Item key="profile" icon={<FontAwesomeIcon icon={faUser} />}>
+                        <Link to="/profile">Hồ sơ người dùng</Link>
+                    </Menu.Item>
+                    <Menu.Item key="cart" icon={<FontAwesomeIcon icon={faShoppingCart} />}>
+                        <Link to="/cart">Giỏ hàng</Link>
+                    </Menu.Item>
+                </Menu>
+            </div>
+            <div className="navigation-right">
+                <Menu mode="horizontal">
+                    <Menu.Item key="login" icon={<FontAwesomeIcon icon={faSignInAlt} />}>
+                        <Link to="/login">Đăng nhập</Link>
+                    </Menu.Item>
+                    <Menu.Item key="signup" icon={<FontAwesomeIcon icon={faSignOutAlt} />}>
+                        <Link to="/register">Đăng ký</Link>
+                    </Menu.Item>
+                </Menu>
+            </div>
+        </div>
     );
 }
 
--- src/features/Customer/Profile/InvoiceDetail/index.jsx
+++ src/features/Customer/Profile/InvoiceDetail/index.jsx
@@ -1,12 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Descriptions, Skeleton, Tag, Spin } from "antd";
+import {
+    CheckCircleOutlined,
+    SyncOutlined,
+    MinusCircleOutlined,
+    LoadingOutlined
+} from '@ant-design/icons';
 import { useParams } from "react-router-dom"
+import NumberFormat from "react-number-format";
+import ProductTable from "../../../../components/ProductTable"
+import moment from "moment";
+import 'moment/locale/vi';
+import "./styles.css"
 
 InvoiceDetail.propTypes = {}
 
+const statusList = [
+    {
+        name: "Hoàn tất",
+        key: "done"
+    },
+    {
+        name: "Đang xử lý",
+        key: "processing"
+    },
+    {
+        name: "Đang giao hàng",
+        key: "delivery"
+    },
+    {
+        name: "Đã hủy",
+        key: "cancel"
+    }
+];
+
 function InvoiceDetail() {
     const { id } = useParams();
-    //1, 'hxiaob46', '697621414', '0706576651', N'Stuart Vi', '373-3720 Mi St.'
-    return (<div>Invoice {id} detail</div>);
+
+    moment.locale('vi');
+
+    const tags = {
+        done: (<Tag icon={<CheckCircleOutlined />} color="success">Hoàn tất</Tag>),
+        processing: (<Tag icon={<SyncOutlined spin />} color="warning">Đang xử lý</Tag>),
+        delivery: (<Tag icon={<SyncOutlined spin />} color="processing">Đang giao hàng</Tag>),
+        cancel: (<Tag icon={<MinusCircleOutlined />} color="error">Đã hủy</Tag>)
+    }
+
+    const [data, setData] = useState({});
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        document.title = "Chi tiết đơn hàng";
+        setIsLoading(true);
+        let fakeData = {
+            invoiceInfo: {
+                key: id, //invoice ID
+                totalPrice: 10000000,
+                createdAt: moment().format('llll'),
+                status: statusList[(Math.floor((Math.random() * 1000)) % statusList.length)].key
+            },
+            invoiceDetail: []
+        };
+
+        for (let i = 0; i < 10; i++)
+            fakeData.invoiceDetail.push({
+                key: i,
+                productName: `Sản phẩm ${i}`,
+                srcImage: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+                price: 10000000,
+                quantity: 1
+            })
+
+        setData(fakeData);
+        setIsLoading(false);
+    }, [id]);
+
+    console.log(data);
+
+    return (
+    <div className="invoice-detail-container">
+        <div className="invoice-information">
+            <Descriptions
+                title="Chi tiết đơn hàng"
+                bordered
+                column={{ xxl: 3, xl: 3, lg: 3, md: 3, sm: 3, xs: 3 }}
+                layout="vertical"
+            >
+                <Descriptions.Item label="Mã đơn hàng" span={1}>
+                    {isLoading ? <Skeleton active /> : data.invoiceInfo.key}
+                </Descriptions.Item>
+                <Descriptions.Item label="Giá trị đơn hàng" span={1}>
+                    {isLoading ? (
+                        <Skeleton active />
+                    ) : (
+                        <>
+                            <NumberFormat
+                                value={data.invoiceInfo.totalPrice}
+                                displayType="text"
+                                thousandSeparator
+                            />
+                            <span style={{ fontSize: "0.75em" }}> VNĐ</span>
+                        </>
+                    )}
+                </Descriptions.Item>
+                <Descriptions.Item label="Ngày lập" span={1}>
+                    {isLoading ? <Skeleton active /> : data.invoiceInfo.createdAt}
+                </Descriptions.Item>
+                <Descriptions.Item label="Trạng thái đơn hàng" span={3}>
+                    {isLoading ? <Skeleton active /> : tags[data.invoiceInfo.status]}
+                </Descriptions.Item>
+            </Descriptions>
+        </div>
+        <div className="invoice-detail-table">
+            {isLoading ? (
+            <div className="loading">
+                <Spin
+                    tip="Đang tải dữ liệu"
+                    indicator={
+                        <LoadingOutlined style={{ fontSize: 100, margin: 50 }} spin />
+                    }
+                />
+            </div>) : (<ProductTable data={data.invoiceDetail} pagination={3} infoOnly/>)}
+        </div>
+    </div>);
 }
 
 export default InvoiceDetail;
