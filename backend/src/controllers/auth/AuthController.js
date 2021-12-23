c7d70d95fef361b308929fd45a8bb9cd1e4c1d54
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-12-23 16:05:20 +0700

    PR2-6.2 - sign in, sign up API

--- backend/src/controllers/auth/AuthController.js
+++ backend/src/controllers/auth/AuthController.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import AuthMiddlewares from "../../middlewares/AuthMiddleware.js";
+import AccountModel from "../../models/AccountModel.js";
+import CustomerModel from "../../models/CustomerModel.js";
 import AppConstants from "../../shared/AppConstants.js";
 import AppController from "../AppController.js";
 
@@ -10,6 +13,10 @@ export default class AuthController extends AppController {
 
     init() {
         this._router.get("/auth/verify", this.verifyToken);
+
+        this._router.post("/auth/login", this.postLogin);
+
+        this._router.post("/auth/signup", AuthMiddlewares.verifySignupData, this.postSignup);
     }
 
     // Every time user access to private page, this handler will be called
@@ -33,5 +40,85 @@ export default class AuthController extends AppController {
 
             return res.json({ status: 200, message: "Verify Ok", data: data.role });
         });
+    }
+
+    async postLogin(req, res) {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.json({ status: 400, message: "Missing data" });
+        }
+
+        try {
+            const users = await AccountModel.getAllByUsernameAndPassword(username, password);
+            if (users.length === 0) {
+                return res.json({ status: 401 });
+            }
+
+            // Login ok
+            // Generate access_token
+            const token = jwt.sign(
+                { username: users[0].TenNguoiDung, role: users[0].VaiTro, id: users[0].MaTK },
+                AppConstants.SECRET_KEY,
+                { expiresIn: "1h" }
+            );
+
+            let expiredIn = new Date();
+            expiredIn.setMinutes(expiredIn.getMinutes() + 60);
+            expiredIn = expiredIn.valueOf();
+
+            return res.json({
+                status: 200,
+                data: {
+                    access_token: token,
+                    expired_in: expiredIn,
+                    role: users[0].VaiTro,
+                },
+            });
+        } catch (error) {
+            console.log(error);
+            return res.json({ status: 500 });
+        }
+    }
+
+    async postSignup(req, res) {
+        const { payload } = res.locals;
+
+        try {
+            const oldUser = await AccountModel.getAllByUsername(payload.username);
+            if (oldUser.length !== 0) {
+                return res.json({ status: 400, message: "Existed user" });
+            }
+
+            const isPhoneValid = await CustomerModel.verifyUniquePhoneNumber(payload.phoneNumber);
+            if (!isPhoneValid) {
+                return res.json({ status: 400, message: "Existed phone number" });
+            }
+
+            // Username is valid
+            // Insert new account
+            const accountId = await AccountModel.insert({
+                TenNguoiDung: payload.username,
+                MatKhau: payload.password,
+                VaiTro: 4,
+            });
+
+            const customer = {
+                TenKH: payload.fullname,
+                SoDienThoai: payload.phoneNumber,
+                MaTK: accountId[0],
+            };
+
+            if (payload.address) {
+                customer.DiaChi = payload.address;
+            }
+
+            await CustomerModel.insert(customer);
+
+            return res.json({ status: 201 });
+        } catch (error) {
+            console.log(error);
+            return res.json({ status: 500 });
+        }
     }
 }
---
+++ backend/src/middlewares/AuthMiddleware.js
@@ -0,0 +1,28 @@
+export default class AuthMiddlewares {
+    static verifySignupData(req, res, next) {
+        const { fullname, phoneNumber, address, username, password } = req.body;
+
+        if (
+            !fullname ||
+            !phoneNumber ||
+            !username ||
+            !password ||
+            !fullname.trim() ||
+            !phoneNumber.trim() ||
+            !username.trim() ||
+            !password.trim()
+        ) {
+            return res.json({ status: 400, message: "Missing data" });
+        }
+
+        res.locals.payload = {
+            fullname,
+            phoneNumber,
+            address,
+            username,
+            password,
+        };
+
+        next();
+    }
+}
